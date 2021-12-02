b1dc42dcd667f7cfaffa250673d7d2fa196e92a4
Bearfootjoe <joseph.heirendt@gmail.com>
2021-12-02 15:18:29 -0600

    header added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+
+Program Author: Joseph Heirendt
+
+USM Id: 10060206
+
+Assignment: Tic Tac Toe GUI
+
+Description:
+
+This app should open up a form and take in a bunch of fields to build two players. 
+Once completed, that form should close and a tic tac toe board should open up and be playable.
+
+*/
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
